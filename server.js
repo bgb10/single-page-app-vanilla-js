fbed6044bced2c74431ca70b6baaf16c53109a71
bgb10 <bgb1028@gmail.com>
2021-07-02 22:55:49 +0900

    video 1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html")); //spa기 때문에 어떤 요청이든 index.html로 오도록 해야함.
 });
 
-app.listen(process.env.PORT || 5060, () => console.log("Server running..."));
+app.listen(process.env.PORT || 8080, () => console.log("Server running..."));
